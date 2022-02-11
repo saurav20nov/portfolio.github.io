f40cb2a2d8c3ae6087d438acc7186cf89cfab9df
SAURAV SUMAN <saurabhsuman20nov@gmail.com>
2022-02-11 21:19:51 +0530

    MY PORTFOLIO

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing",{
+    strings:["","Software  Developer","Web Developer","Blogger","Data Analyser"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
